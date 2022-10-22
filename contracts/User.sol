9ed0fb41b8161ba54ffe94df4f883703048111e7
Christian <chmereles@gmail.com>
2022-10-22 19:37:28 -0300

    Updated User

--- contracts/User.sol
+++ contracts/User.sol
@@ -3,6 +3,10 @@
 
 pragma solidity ^0.8.17;
 
+/**
+ * @dev Contrato base para los usuario de la dApp
+ *
+ */
 contract User {
     string public name;
 
@@ -11,13 +15,25 @@ contract User {
     }
 }
 
+/**
+ * @dev Contrato que representa los usuarios que tengan responsabilidad de pagar expensas
+ *
+ */
 contract Payer is User {
     constructor(string memory _name) User(_name) {}
 }
 
+/**
+ * @dev Contrato que representa los usuarios que tengan que recibir un pago
+ *
+ */
 contract Employee is User {
     constructor(string memory _name) User(_name) {}
 
+    /**
+     * @dev Permitir que el contrato pueda recibir dinero
+     *
+     */
     receive() external payable {}
 
     function getBalance() external view returns (uint256) {
